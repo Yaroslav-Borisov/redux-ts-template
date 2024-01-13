e47e56a6bf9723c8d76273bdcec65e92e2f8e898
Yaroslav-Borisov <yarpolar@gmail.com>
2024-01-13 22:35:22 +0300

    Подключит сервер и обработает мутации

--- src/components/PostContainer.tsx
+++ src/components/PostContainer.tsx
@@ -1,10 +1,20 @@
+import { IPost } from "../models/IPost"
 import { postAPI } from "../services/PostService"
 import { PostItem } from "./PostItem"
 
 export const PostContainer = () => {
-    const {data: posts, error, isLoading} = postAPI.useFetchAllPostsQuery(5)
+    const {data: posts, error, isLoading} = postAPI.useFetchAllPostsQuery(100)
+    // eslint-disable-next-line no-empty-pattern
+    const [createPost, {}] = postAPI.useCreatePostMutation()
+
+    const handleCreate = async () => {
+        const title = prompt()
+        await createPost({title, body: 'yarik'} as IPost)
+    }
+    
     return (
         <div>
+            <button onClick={handleCreate}>Add new post</button>
             {isLoading && <h3>Идёт загрузка постов...</h3>}
             {error && <h3>Произошла ошибка...</h3>}
             {posts && posts.map(post => <PostItem post={post} key={post.id}/>)}
--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -1,14 +1,23 @@
 import { IPost } from "../models/IPost"
+import { postAPI } from "../services/PostService"
 
 interface PostItemProps {
     post: IPost
 }
 
 export const PostItem = ({post}: PostItemProps) => {
+    // eslint-disable-next-line no-empty-pattern
+    const [deletePost, {}] = postAPI.useDeletePostMutation()
+    
+    const handleClick = async (event: React.MouseEvent) => {
+        event.stopPropagation()
+        await deletePost(post)
+    }
+    
     return (
         <div className="post">
             {post.id}. {post.title}
-            <button>Delete</button>
+            <button onClick={handleClick}>Delete</button>
         </div>
     )
 }
